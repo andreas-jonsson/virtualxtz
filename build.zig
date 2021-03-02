ed75e827ffbf425238b8d151befa65f475c265ef
Andreas T Jonsson <mail@andreasjonsson.se>
2021-03-02 09:58:30 +0100

    Just a backup

--- build.zig
+++ build.zig
@@ -4,21 +4,6 @@ const Target = std.build.Target;
 
 pub fn build(b: *Builder) void {
     const mode = b.standardReleaseOptions();
-    //const target = b.standardTargetOptions(.{
-    //    .whitelist = &[_]Target{
-    //        .{
-    //            .cpu_arch = .i386,
-    //            .os_tag = .windows,
-    //            .abi = .gnu,
-    //        },
-    //        .{
-    //            .cpu_arch = .x86_64,
-    //            .os_tag = .windows,
-    //            .abi = .gnu,
-    //        },
-    //    },
-    //});
-
     const exe = b.addExecutable("virtualxt", "src/main.zig");
 
     const sdl_path = b.option([]const u8, "sdl-path", "Path to SDL2 headers and libs") orelse null; 
@@ -32,12 +17,11 @@ pub fn build(b: *Builder) void {
     exe.linkSystemLibrary("c");
     exe.linkSystemLibrary("SDL2");
 
-    //exe.setTarget(target);
     exe.setBuildMode(mode);
-    //exe.addPackage(.{
-    //    .name = "libvxt",
-    //    .path = "libs/libvxt/libvxt.zig",
-    //});
+    exe.addPackage(.{
+        .name = "vxt",
+        .path = "libs/vxt/vxt.zig",
+    });
 
     const run_cmd = exe.run();
     const run_step = b.step("run", "Run VirtualXT");
--- src/main.zig
+++ src/main.zig
@@ -1,36 +1,71 @@
 const std = @import("std");
-//const vxt = @import("libvxt");
+const vxt = @import("vxt");
 
-const sdl = @cImport({
+const c = @cImport({
     @cInclude("SDL2/SDL.h");
 });
 
 const ErrorSet = error{SDLError};
 
 pub fn main() !void {
-    if (sdl.SDL_Init(sdl.SDL_INIT_VIDEO | sdl.SDL_INIT_AUDIO) != 0) {
-        std.debug.warn("Unable to initialize SDL: {s}", sdl.SDL_GetError());
+    if (c.SDL_Init(c.SDL_INIT_VIDEO | c.SDL_INIT_AUDIO) != 0) {
+        std.debug.warn("Unable to initialize SDL: {s}", .{c.SDL_GetError()});
         return ErrorSet.SDLError;
     }
-    defer sdl.SDL_Quit();
+    defer c.SDL_Quit();
 
-    const screen_width = 160;//vxt.the_number();
+    _ = c.SDL_SetHint(c.SDL_HINT_RENDER_SCALE_QUALITY, "0");
+    _ = c.SDL_SetHint(c.SDL_HINT_WINDOWS_NO_CLOSE_ON_ALT_F4, "1");
+
+    const screen_width = vxt.the_number();
     const screen_height = 144;
-    const undefined_pos = 0x1FFF0000;
 
-    const window = sdl.SDL_CreateWindow(
+    const window = c.SDL_CreateWindow(
         "VirtualXT",
-        undefined_pos,
-        undefined_pos,
-        screen_width,
-        screen_height,
-        sdl.SDL_WINDOW_SHOWN,
-    );
-    if (window == null) {
-        std.debug.warn("Cannot create window: {s}", sdl.SDL_GetError());
+        c.SDL_WINDOWPOS_UNDEFINED_MASK,
+        c.SDL_WINDOWPOS_UNDEFINED_MASK,
+        640,
+        480,
+        c.SDL_WINDOW_RESIZABLE,
+    ) orelse {
+        std.debug.warn("Cannot create window: {s}", .{c.SDL_GetError()});
         return ErrorSet.SDLError;
+    };
+    defer c.SDL_DestroyWindow(window);
+
+    c.SDL_SetWindowTitle(window, "VirtualXT");
+
+    const renderer = c.SDL_CreateRenderer(window, -1, 0) orelse {
+        std.debug.warn("Cannot create renderer: {s}", .{c.SDL_GetError()});
+        return ErrorSet.SDLError;
+    };
+    defer c.SDL_DestroyRenderer(renderer);
+
+    const texture = c.SDL_CreateTexture(renderer, c.SDL_PIXELFORMAT_ABGR8888, c.SDL_TEXTUREACCESS_STREAMING, 640, 200) orelse {
+        std.debug.warn("Cannot create texture: {s}", .{c.SDL_GetError()});
+        return ErrorSet.SDLError;
+    };
+    defer c.SDL_DestroyTexture(texture);
+
+    if (c.SDL_RenderSetLogicalSize(renderer, 640, 480) != 0) {
+        std.debug.warn("Unable to set logical renderer size: {s}", .{c.SDL_GetError()});
     }
-    defer sdl.SDL_DestroyWindow(window);
-    const screen = sdl.SDL_GetWindowSurface(window);
-    std.os.nanosleep(5, 0);
+
+    while (true) {
+        var event: c.SDL_Event = undefined;
+        while (c.SDL_PollEvent(&event) != 0) {
+            switch (event.@"type") {
+                c.SDL_QUIT => {
+                    return;
+                },
+                else => {},
+            }
+        }
+
+        _ = c.SDL_RenderClear(renderer);
+        _ = c.SDL_RenderCopy(renderer, texture, null, null);
+        c.SDL_RenderPresent(renderer);
+
+        std.time.sleep(16*1000000);
+    }
 }
